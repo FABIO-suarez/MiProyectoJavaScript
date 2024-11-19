104995be0df184e2ded68d26d61ee62fc5eb2a96
GERALDINEwasas <rgguzman71@gmail.com>
2024-11-19 14:17:59 -0400

    FabioJavaScript

---
+++ JavaScript/main.js
@@ -0,0 +1,32 @@
+function sumar()
+{
+let numeroUno=2;
+let numeroDos=4;
+let numeroTres=10;
+let resultado=numeroUno+numeroDos+numeroTres;
+alert("la suma es "+ resultado)
+}
+function resta()
+{
+let numeroUno=2;
+let numeroDos=4;
+let numeroTres=10;
+let resultado=numeroUno-numeroDos-numeroTres;
+alert("la resta es "+ resultado)
+}
+function multiplicar()
+{
+let numeroUno=2;
+let numeroDos=4;
+let numeroTres=10;
+let resultado=numeroUno*numeroDos*numeroTres;
+alert("la multiplicacion es "+ resultado)
+}
+function division()
+{
+let numeroUno=2;
+let numeroDos=4;
+let numeroTres=10;
+let resultado=numeroUno/numeroDos/numeroTres;
+alert("la division es "+ resultado)
+}
